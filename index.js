6d564d5154d3a7200ac3f21790b2b0079cd33551
Samantha Jordan <shinyguns@gmail.com>
2021-04-02 14:02:20 -0500

    done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+function findMatching(driversArray, queryString){
+    const matched = driversArray.filter(
+        driver => {
+            return driver.toLowerCase() === 
+            queryString.toLowerCase()
+        }
+    )
+    return matched;
+}
+
+function fuzzyMatch(driverList, chars) {
+    return driverList.filter(
+        driver => 
+        driver.indexOf(chars) === 
+        chars.indexOf(chars)
+        )
+    }
+
+function matchName(driversArray, queryString){
+    let matchingNames = driversArray.filter(
+        driver => 
+            driver.name === queryString)
+            return matchingNames
+        }
